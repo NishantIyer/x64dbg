d9d8a0f2a2a29f4ea4c95279961918674b64456e
NishantIyer <nishant.iyer2609@outlook.com>
2023-06-03 11:40:32 +0000

    Commit 13

---
+++ 2cafe072d1da2972.asm
@@ -0,0 +1,39 @@
+; Block 1 - 2cafe072d1da2972.asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_1
+jmp exit
+label_1:
+mov ecx, 4
+mul ecx
+exit:
+
+; Block 2 - 2cafe072d1da2972.asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_2
+jmp exit
+label_2:
+mov ecx, 4
+mul ecx
+exit:
+
+; Block 3 - 2cafe072d1da2972.asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_3
+jmp exit
+label_3:
+mov ecx, 4
+mul ecx
+exit:
+

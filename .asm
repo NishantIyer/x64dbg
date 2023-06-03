897bb855467e7161c38e0f14c6ee7390ffef4b23
NishantIyer <nishant.iyer2609@outlook.com>
2023-06-03 11:38:16 +0000

    Commit 2

--- .asm
+++ .asm
@@ -37,3 +37,42 @@ mov ecx, 4
 mul ecx
 exit:
 
+; Block 1 - .asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_1
+jmp exit
+label_1:
+mov ecx, 4
+mul ecx
+exit:
+
+; Block 2 - .asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_2
+jmp exit
+label_2:
+mov ecx, 4
+mul ecx
+exit:
+
+; Block 3 - .asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_3
+jmp exit
+label_3:
+mov ecx, 4
+mul ecx
+exit:
+
